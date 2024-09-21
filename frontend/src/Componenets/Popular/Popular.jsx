c49c698d26d814376e116b6043ecd07540022833
arpitgit00 <arpitparmar0123@gmail.com>
2024-09-21 11:04:26 +0530

    ok

--- frontend/src/Componenets/Popular/Popular.jsx
+++ frontend/src/Componenets/Popular/Popular.jsx
@@ -8,7 +8,7 @@ function Popular() {
   const [popularProducts,setPopularProducts] = useState([]);
 
   useEffect(()=>{
-    fetch('http://localhost:4000/popularinwomen')
+    fetch('https://ecommback-gamma.vercel.app/popularinwomen')
     .then((response)=>response.json())
     .then((data)=>setPopularProducts(data));
   },[])
